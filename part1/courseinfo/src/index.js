cf0415737f8ddf2d736c6e335ffa4d43dfed72d7
Moises Camacho <moisescamacho26@gmail.com>
2021-05-10 16:38:56 -0400

    Exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,17 +10,11 @@ const Header = (props)=>{
 
 const Content=(props)=>{
     return (
-        <>
-            <p>
-                {props.part1} {props.exercise1}
-            </p>
-            <p>
-                {props.part2} {props.exercise2}
-            </p>
-            <p>
-                {props.part3} {props.exercise3}
-            </p>
-        </>
+        <div>
+            < Part part={props.part1} exercise={props.exercise1}/>
+            < Part part={props.part2} exercise={props.exercise2}/>            
+            < Part part={props.part3} exercise={props.exercise3}/>            
+        </div>
     )
 }
 
@@ -32,6 +26,11 @@ const Total= (props)=>{
     )
 }
 
+const Part= (props)=>{
+    return (
+        <p> {props.part}  {props.exercise} </p>
+    )
+}
 const App = ()=>{
     const course = 'Half Stack applicalction development'
     const part1='Fundamental of React'
