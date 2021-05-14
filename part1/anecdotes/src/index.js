62ceb44b7b84b57879efbeea4bf7b4ac7e090b51
Moises Camacho <moisescamacho26@gmail.com>
2021-05-13 20:06:41 -0400

    Exercise 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Anecdotes=({anecdotes, points})=><><div>{anecdotes}</div><div>has {points} votes</div> </>
+
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [points, setPoints]=useState(new Array(6+1).join('0').split('').map(parseFloat));
   const copy=[...points];
+  let max= copy.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)
   const handleClick= (i)=> {
     copy[i]+=1;
+    max=copy.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)
     setPoints(copy);
   }
   const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
@@ -17,10 +21,12 @@ const App = (props) => {
 
   return (
     <div>
-      <div>{props.anecdotes[selected]}</div>
-      <div> has {copy[selected]} votes </div>
+      <h1> Anecdote of the day </h1>
+      <Anecdotes anecdotes={props.anecdotes[selected]} points={points[selected]}/>
       <button onClick={()=>handleClick(selected)} > vote </button>
       <button onClick={RandomAnectdotes}> next anecdote </button>
+      <h1> Anecdote with most votes </h1>
+      <Anecdotes anecdotes={props.anecdotes[max]} points={points[max]}/>
     </div>
   );
 };
